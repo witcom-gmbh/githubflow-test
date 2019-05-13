21e9bd1b7ecea3927bac9d573af97ca6adf5f293
Carsten Buchberger <c.buchberger@witcom.de>
2019-05-13 15:26:54 +0000

    dfgdg

--- complete/src/main/java/hello/HelloController.java
+++ complete/src/main/java/hello/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
     
     @RequestMapping("/")
     public String index() {
-        return "Greetinxxxxxx from Spring Boot!";
+        return "Greetinxxxxxxx from Spring Boot!";
     }
     
 }
