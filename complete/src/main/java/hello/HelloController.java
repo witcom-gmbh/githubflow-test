15304b0ea8799d833a5a8588b40d65cf1f2dc2ca
Carsten Buchberger <c.buchberger@witcom.de>
2019-05-13 11:38:18 +0000

    Noch ein Versuch

--- complete/src/main/java/hello/HelloController.java
+++ complete/src/main/java/hello/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
     
     @RequestMapping("/")
     public String index() {
-        return "Greetinxxxx from Spring Boot!";
+        return "Greetinx from Spring Boot!";
     }
     
 }
