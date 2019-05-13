d0610ad06da6c71352f046dd1e14b30fe2722d84
Carsten Buchberger <c.buchberger@witcom.de>
2019-05-13 11:17:03 +0000

    Trigger PR-update

--- complete/src/main/java/hello/HelloController.java
+++ complete/src/main/java/hello/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
     
     @RequestMapping("/")
     public String index() {
-        return "Greetinx from Spring Boot!";
+        return "Greetinxx from Spring Boot!";
     }
     
 }
