d1c130ccfe0c4459363bffc5e3ce3d40f1e10473
Carsten Buchberger <c.buchberger@witcom.de>
2019-05-20 15:51:23 +0000

    next bug

--- complete/src/main/java/hello/HelloController.java
+++ complete/src/main/java/hello/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
     
     @RequestMapping("/")
     public String index() {
-        return "Greetinxs from Spring Boot!";
+        return "Greetinxx from Spring Boot!";
     }
     
 }
